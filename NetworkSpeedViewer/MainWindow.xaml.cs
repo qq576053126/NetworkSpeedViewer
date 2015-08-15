e0630ffc1bd59b23e9a8f65dbf3f95faa426a20a
Removable <ftrftr@vip.qq.com>
2015-08-15 13:30:17 +0800

    窗体透明设置

--- NetworkSpeedViewer/MainWindow.xaml.cs
+++ NetworkSpeedViewer/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace NetworkSpeedViewer
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
     }
 }

88e74e083bea579446b8b37004f602d13c10a081
Removable <ftrftr@vip.qq.com>
2015-08-16 01:28:45 +0800

    完成设置窗口功能

--- NetworkSpeedViewer/MainWindow.xaml.cs
+++ NetworkSpeedViewer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Echevil;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using MyConfigure;
 
 namespace NetworkSpeedViewer
 {
@@ -19,14 +22,44 @@ namespace NetworkSpeedViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NetworkAdapter[] adapters;
+        private NetworkMonitor monitor;
         public MainWindow()
         {
             InitializeComponent();
         }
-
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.adapters = null;
+            this.adapters = FoundNetworkAdapter();
+            if (adapters.Length == 0)
+            {
+                MessageBox.Show("未在本机找到网络适配器！请检查硬件或者相应驱动是否正确安装");
+                return;
+            }
+            if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini") || ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null") == "$null")
+            {
+                MessageBox.Show("您的电脑上存在多个网络适配器，请选择当前使用的适配器");
+                if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini"))
+                    File.Create(Environment.CurrentDirectory + @"/settings.ini");
+                ConfigureFile.INIWriteItems(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter=$null");
+                SettingsWin sw = SettingsWin.GetInstance();
+                sw.Show();
+            }
+        }
+
+        private NetworkAdapter[] FoundNetworkAdapter()
+        {
+            NetworkAdapter[] adaptersTemp;
+            monitor = new NetworkMonitor();
+            adaptersTemp = monitor.Adapters;
+            return adaptersTemp;
+        }
+
     }
 }

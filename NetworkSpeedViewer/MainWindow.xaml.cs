45fa027a2a6682d690dad65f79a4998bbc522c01
Removable <ftrftr@vip.qq.com>
2015-08-20 00:14:32 +0800

    初步完成功能

--- NetworkSpeedViewer/MainWindow.xaml.cs
+++ NetworkSpeedViewer/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using MyConfigure;
+using System.Threading;
 
 namespace NetworkSpeedViewer
 {
@@ -23,10 +24,61 @@ namespace NetworkSpeedViewer
     public partial class MainWindow : Window
     {
         private NetworkAdapter[] adapters;
+        private NetworkAdapter adapterTemp;
         private NetworkMonitor monitor;
+        System.Timers.Timer swTimer = new System.Timers.Timer();
+        System.Timers.Timer speedTimer = new System.Timers.Timer();
         public MainWindow()
         {
             InitializeComponent();
+            swTimer.Interval = 1000;
+            swTimer.Elapsed += swTimer_Elapsed;
+            speedTimer.Interval = 1000;
+            speedTimer.Elapsed += speedTimer_Elapsed;
+        }
+
+        void speedTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            NetworkAdapter adapter = adapterTemp;
+            long downLoadSpeed = adapter.DownloadSpeed;
+            new Thread(() =>
+            {
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    //下载速度显示
+                    if (downLoadSpeed < 1024)
+                        mainDownload.Text = NumberToBit(downLoadSpeed);
+                    if (downLoadSpeed >= 1024 && downLoadSpeed < 1024 * 1024)
+                        mainDownload.Text = NumberToKB(Convert.ToDouble(downLoadSpeed));
+                    if (downLoadSpeed >= 1024 * 1024)
+                        mainDownload.Text = NumberToMB(Convert.ToDouble(downLoadSpeed));
+                    //上载速度显示
+                    long upLoadSpeed = adapter.UploadSpeed;
+                    if (upLoadSpeed < 1024)
+                        mainUpload.Text = NumberToBit(upLoadSpeed);
+                    if (upLoadSpeed >= 1024 && upLoadSpeed < 1024 * 1024)
+                        mainUpload.Text = NumberToKB(Convert.ToDouble(upLoadSpeed));
+                    if (upLoadSpeed >= 1024 * 1024)
+                        mainUpload.Text = NumberToMB(Convert.ToDouble(upLoadSpeed));
+                }));
+            }).Start();
+        }
+
+        void swTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            SettingsWin sw = SettingsWin.GetInstance();
+            new Thread(() =>
+            {
+                this.Dispatcher.Invoke(new Action(() =>
+                {
+                    if (!sw.IsVisible)
+                    {
+                        MessageBox.Show("1");
+                        swTimer.Stop();
+                    }
+                }));
+            }).Start();
+
         }
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -42,7 +94,7 @@ namespace NetworkSpeedViewer
                 MessageBox.Show("未在本机找到网络适配器！请检查硬件或者相应驱动是否正确安装");
                 return;
             }
-            if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini") || ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null") == "$null")
+            else if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini") || ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null") == "$null")
             {
                 MessageBox.Show("您的电脑上存在多个网络适配器，请选择当前使用的适配器");
                 if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini"))
@@ -50,6 +102,22 @@ namespace NetworkSpeedViewer
                 ConfigureFile.INIWriteItems(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter=$null");
                 SettingsWin sw = SettingsWin.GetInstance();
                 sw.Show();
+                swTimer.Start();
+            }
+            else
+            {
+                string selectedAdapter = ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null");
+                foreach (NetworkAdapter item in this.adapters)
+                {
+                    if (item.Name == selectedAdapter)
+                    {
+                        adapterTemp = item;
+                        monitor.StopMonitoring();
+                        monitor.StartMonitoring(item);
+                        speedTimer.Start();
+                        break;
+                    }
+                }
             }
         }
 
@@ -60,6 +128,29 @@ namespace NetworkSpeedViewer
             adaptersTemp = monitor.Adapters;
             return adaptersTemp;
         }
-
+        //格式转换
+        private string NumberToBit(long speed)
+        {
+            return RemoveZore(String.Format("{0:N}", speed)) + "B/秒";
+        }
+        private string NumberToKB(double speed)
+        {
+            return RemoveZore(String.Format("{0:N}", (speed / 1024))) + "KB/秒";
+        }
+        private string NumberToMB(double speed)
+        {
+            return RemoveZore(String.Format("{0:N}", speed / (1024 * 1024))) + "MB/秒";
+        }
+        private string RemoveZore(string s)
+        {
+            if (s.Substring(s.Length - 3) == ".00")
+            {
+                return s.Substring(0, s.Length - 3);
+            }
+            else if (s.Substring(s.Length - 1) == "0")
+                return s.Substring(0, s.Length - 1);
+            else
+                return s;
+        }
     }
 }

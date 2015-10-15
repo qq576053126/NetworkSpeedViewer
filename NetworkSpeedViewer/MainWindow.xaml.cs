2bff2fcedab0d4b70e998bc27d80afcf90415c54
Removable <ftrftr@vip.qq.com>
2015-10-15 18:57:03 +0800

    格式化代码

--- NetworkSpeedViewer/MainWindow.xaml.cs
+++ NetworkSpeedViewer/MainWindow.xaml.cs
@@ -1,41 +1,42 @@
-﻿using Echevil;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
+﻿#region
+
+using Echevil;
 using MyConfigure;
+using System;
+using System.ComponentModel;
+using System.IO;
 using System.Threading;
+using System.Timers;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.MessageBox;
+using Timer = System.Timers.Timer;
+
+#endregion
 
 namespace NetworkSpeedViewer
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    ///     MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
-        private NetworkAdapter[] adapters;
-        private NetworkAdapter adapterTemp;
-        private NetworkMonitor monitor;
-        private System.Timers.Timer swTimer = new System.Timers.Timer();
-        private System.Timers.Timer speedTimer = new System.Timers.Timer();
-        private System.Windows.Forms.NotifyIcon nofityIcon = null;
+        private readonly Timer _speedTimer = new Timer();
+        private readonly Timer _swTimer = new Timer();
+        private NetworkAdapter[] _adapters;
+        private NetworkAdapter _adapterTemp;
+        private NetworkMonitor _monitor;
+        private NotifyIcon _nofityIcon;
+
         public MainWindow()
         {
             InitializeComponent();
-            swTimer.Interval = 1000;
-            swTimer.Elapsed += swTimer_Elapsed;
-            speedTimer.Interval = 1000;
-            speedTimer.Elapsed += speedTimer_Elapsed;
+            _swTimer.Interval = 1000;
+            _swTimer.Elapsed += swTimer_Elapsed;
+            _speedTimer.Interval = 1000;
+            _speedTimer.Elapsed += speedTimer_Elapsed;
 
             NotifyIconSettings();
         }
@@ -43,60 +44,60 @@ namespace NetworkSpeedViewer
         private void NotifyIconSettings()
         {
             //托盘
-            this.nofityIcon = new System.Windows.Forms.NotifyIcon();
-            this.nofityIcon.Icon = Properties.Resources.meter_128px;
-            this.nofityIcon.Text = "程序正在运行";
-            this.nofityIcon.Visible = true;
+            _nofityIcon = new NotifyIcon();
+            _nofityIcon.Icon = Properties.Resources.meter_128px;
+            _nofityIcon.Text = "程序正在运行";
+            _nofityIcon.Visible = true;
             //菜单项
-            System.Windows.Forms.MenuItem settingsMenu = new System.Windows.Forms.MenuItem("设置");
-            System.Windows.Forms.MenuItem exitMenu = new System.Windows.Forms.MenuItem("退出");
+            MenuItem settingsMenu = new MenuItem("设置");
+            MenuItem exitMenu = new MenuItem("退出");
             exitMenu.Click += exitMenu_Click;
-            System.Windows.Forms.MenuItem[] menuItems = new System.Windows.Forms.MenuItem[] { settingsMenu, exitMenu };
-            this.nofityIcon.ContextMenu = new System.Windows.Forms.ContextMenu(menuItems);
+            MenuItem[] menuItems = { settingsMenu, exitMenu };
+            _nofityIcon.ContextMenu = new ContextMenu(menuItems);
         }
 
         //托盘退出按钮
-        void exitMenu_Click(object sender, EventArgs e)
+        private void exitMenu_Click(object sender, EventArgs e)
         {
             Application.Current.Shutdown();
         }
 
         //速度刷新计时器方法
-        void speedTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private void speedTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            NetworkAdapter adapter = adapterTemp;
+            NetworkAdapter adapter = _adapterTemp;
             new Thread(() =>
             {
-                this.Dispatcher.Invoke(new Action(() =>
+                Dispatcher.Invoke(new Action(() =>
                 {
                     //下载速度显示
-                    mainDownload.Text = Utility.NumberToDlSpeed(adapter.DownloadSpeed);
+                    MainDownload.Text = Utility.NumberToDlSpeed(adapter.DownloadSpeed);
                     //上载速度显示
-                    mainUpload.Text = Utility.NumberToUlSpeed(adapter.UploadSpeed);
+                    MainUpload.Text = Utility.NumberToUlSpeed(adapter.UploadSpeed);
                 }));
             }).Start();
         }
 
         //探测设置窗口是否关闭的计时器方法
-        void swTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private void swTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             SettingsWin sw = SettingsWin.GetInstance();
             new Thread(() =>
             {
-                this.Dispatcher.Invoke(new Action(() =>
+                Dispatcher.Invoke(new Action(() =>
                 {
                     if (!sw.IsVisible)
                     {
-                        mainFunction();
-                        swTimer.Stop();
+                        MainFunction();
+                        _swTimer.Stop();
                     }
                 }));
             }).Start();
-
         }
+
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove();
+            DragMove();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -104,29 +105,34 @@ namespace NetworkSpeedViewer
             double screenHeight = SystemParameters.PrimaryScreenHeight;
             double screenWidth = SystemParameters.PrimaryScreenWidth;
 
-            double saveLeft = Convert.ToDouble(MyConfigure.ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinLeft", "-1000"));
-            double saveTop = Convert.ToDouble(MyConfigure.ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinTop", "-1000"));
+            double saveLeft =
+                Convert.ToDouble(ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini",
+                    "ProgramSettings", "WinLeft", "-1000"));
+            double saveTop =
+                Convert.ToDouble(ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini",
+                    "ProgramSettings", "WinTop", "-1000"));
 
-            if (saveLeft == -1000 || saveLeft >= screenWidth - 2 || saveLeft <= -this.Width + 2)
-                saveLeft = screenWidth - this.Width;
-            if (saveTop == -1000 || saveTop >= screenHeight - 2 || saveTop <= -this.Top + 2)
-                saveTop = screenHeight - this.Height - (screenHeight / 20);
+            if (saveLeft == -1000 || saveLeft >= screenWidth - 2 || saveLeft <= -Width + 2)
+                saveLeft = screenWidth - Width;
+            if (saveTop == -1000 || saveTop >= screenHeight - 2 || saveTop <= -Top + 2)
+                saveTop = screenHeight - Height - (screenHeight / 20);
 
-            this.WindowStartupLocation = WindowStartupLocation.Manual;
-            this.Left = saveLeft;
-            this.Top = saveTop;
-            mainFunction();
+            WindowStartupLocation = WindowStartupLocation.Manual;
+            Left = saveLeft;
+            Top = saveTop;
+            MainFunction();
         }
-        private void mainFunction()
+
+        private void MainFunction()
         {
-            this.adapters = null;
-            this.adapters = FoundNetworkAdapter();
-            if (adapters.Length == 0)
+            _adapters = null;
+            _adapters = FoundNetworkAdapter();
+            if (_adapters.Length == 0)
             {
                 MessageBox.Show("未在本机找到网络适配器！请检查硬件或者相应驱动是否正确安装");
-                return;
             }
-            else if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini") || ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null") == "$null")
+            if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini") ||
+                ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null") == "$null")
             {
                 MessageBox.Show("您的电脑上存在多个网络适配器，请选择当前使用的适配器");
                 if (!File.Exists(Environment.CurrentDirectory + @"/settings.ini"))
@@ -134,19 +140,20 @@ namespace NetworkSpeedViewer
                 ConfigureFile.INIWriteItems(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter=$null");
                 SettingsWin sw = SettingsWin.GetInstance();
                 sw.Show();
-                swTimer.Start();
+                _swTimer.Start();
             }
             else
             {
-                string selectedAdapter = ConfigureFile.INIGetStringValue(Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null");
-                foreach (NetworkAdapter item in this.adapters)
+                string selectedAdapter = ConfigureFile.INIGetStringValue(
+                    Environment.CurrentDirectory + @"/settings.ini", "AdapterProperty", "SelectedAdapter", "$null");
+                foreach (NetworkAdapter item in _adapters)
                 {
                     if (item.Name == selectedAdapter)
                     {
-                        adapterTemp = item;
-                        monitor.StopMonitoring();
-                        monitor.StartMonitoring(item);
-                        speedTimer.Start();
+                        _adapterTemp = item;
+                        _monitor.StopMonitoring();
+                        _monitor.StartMonitoring(item);
+                        _speedTimer.Start();
                         break;
                     }
                 }
@@ -155,24 +162,22 @@ namespace NetworkSpeedViewer
 
         private NetworkAdapter[] FoundNetworkAdapter()
         {
-            NetworkAdapter[] adaptersTemp;
-            monitor = new NetworkMonitor();
-            adaptersTemp = monitor.Adapters;
+            _monitor = new NetworkMonitor();
+            var adaptersTemp = _monitor.Adapters;
             return adaptersTemp;
         }
 
         private void Window_LocationChanged(object sender, EventArgs e)
         {
-            MyConfigure.ConfigureFile.INIWriteValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinLeft", this.Left.ToString());
-            MyConfigure.ConfigureFile.INIWriteValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinTop", this.Top.ToString());
+            ConfigureFile.INIWriteValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinLeft", Left.ToString());
+            ConfigureFile.INIWriteValue(Environment.CurrentDirectory + @"/settings.ini", "ProgramSettings", "WinTop", Top.ToString());
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        private void Window_Closing(object sender, CancelEventArgs e)
         {
-            this.nofityIcon.Visible = false;
-            this.nofityIcon.Dispose();
-            this.nofityIcon = null;
+            _nofityIcon.Visible = false;
+            _nofityIcon.Dispose();
+            _nofityIcon = null;
         }
-
     }
 }
